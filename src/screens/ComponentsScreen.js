aa9aadb04c7f7c2851060b0138658837e78714bb
kmarks2013 <kmarks2013@gmail.com>
2021-01-27 19:45:24 -0500

    adds high there to show grouping and default text size

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -4,6 +4,7 @@ import { Text, StyleSheet, View } from 'react-native'
 const ComponentsScreen = () => {
     return <View>
             <Text style={styles.textStyle} >This is the components screen</Text>
+            <Text>Hi there!</Text>
     </View>
 }
 
