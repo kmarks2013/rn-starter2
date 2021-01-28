5938df606f50fc52b9997673ed8dbf537c1fff17
kmarks2013 <kmarks2013@gmail.com>
2021-01-27 19:43:45 -0500

    imports teh view element

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 
 const ComponentsScreen = () => {
     return <Text style={styles.textStyle} >This is the components screen</Text>
