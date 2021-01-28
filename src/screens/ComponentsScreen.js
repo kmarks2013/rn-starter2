5edc63e5a704948cd83ef4eda94e279954e3187a
kmarks2013 <kmarks2013@gmail.com>
2021-01-27 19:27:41 -0500

    adds styling to text element

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native'
 
 const ComponentsScreen = () => {
-    return <Text>This is the components screen</Text>
+    return <Text style={styles.textStyle}>This is the components screen</Text>
 }
 
 const styles = StyleSheet.create({
