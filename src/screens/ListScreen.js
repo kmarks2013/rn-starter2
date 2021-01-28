b8d895795e96cea3d5eeb9d0bf1142ae2c6a43cf
kmarks2013 <kmarks2013@gmail.com>
2021-01-28 18:54:46 -0500

    adds ages to each friend

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,15 +3,15 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
     const friends = [
-        { name: "Friend #1"/*, key: "1" <--Option 1 for adding a key*/},
-        { name: "Friend #2"},
-        { name: "Friend #3"},
-        { name: "Friend #4"},
-        { name: "Friend #5"},
-        { name: "Friend #6"},
-        { name: "Friend #7"},
-        { name: "Friend #8"},
-        { name: "Friend #9"}
+        { name: "Friend #1", age: 20 /*, key: "1" <--Option 1 for adding a key*/},
+        { name: "Friend #2", age: 45},
+        { name: "Friend #3", age: 32},
+        { name: "Friend #4", age: 27},
+        { name: "Friend #5", age: 53},
+        { name: "Friend #6", age: 24},
+        { name: "Friend #7", age: 33},
+        { name: "Friend #8", age: 32},
+        { name: "Friend #9", age: 23}
     ]
 
     return (
