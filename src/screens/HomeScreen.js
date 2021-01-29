cade8b0a192d0091a6b6f98e18e0c125b013f03b
kmarks2013 <kmarks2013@gmail.com>
2021-01-28 19:06:03 -0500

    wraps text tag with a view tag

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,11 @@ import React from "react";
 import {View, Text, StyleSheet, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}> Hi there!</Text>;
+  return(
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
