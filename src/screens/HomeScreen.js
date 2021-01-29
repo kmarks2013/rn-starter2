5a2beb82f9c5dc067e9df366288fda8393754f4a
kmarks2013 <kmarks2013@gmail.com>
2021-01-28 19:11:01 -0500

    imports touchable opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, StyleSheet, Button } from "react-native";
+import {View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return(
