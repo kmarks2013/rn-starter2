db38e24b58f1c910845e3f6382123e0c7d50e44a
kmarks2013 <kmarks2013@gmail.com>
2021-01-28 19:56:26 -0500

    adds image prop to image detail

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,7 +5,7 @@ import ImageDetail from '../components/ImageDetail'
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail title="Forest" />
+            <ImageDetail title="Forest" image="../../assets/forest.jpg"/>
             <ImageDetail title="Beach"/>
             <ImageDetail title="Mountain"/>
         </View>
