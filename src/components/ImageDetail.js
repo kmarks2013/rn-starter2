3ac372231fae344dcaa9ecf254ae3147455bbeda
kmarks2013 <kmarks2013@gmail.com>
2021-01-28 19:58:15 -0500

    adds imageSource prop

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 
-const ImageDetail = ( {title} ) => {
+const ImageDetail = ( {title, imageSource} ) => {
 
     return (
         <View>
