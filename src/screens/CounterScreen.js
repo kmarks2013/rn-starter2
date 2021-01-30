e1f1479abe4d7d5ce3a72a784cf5d2581d69af6d
kmarks2013 <kmarks2013@gmail.com>
2021-01-30 18:53:41 -0500

    adds broiler pate text

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const  CounterScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default  CounterScreen
+
+const styles = StyleSheet.create({})
