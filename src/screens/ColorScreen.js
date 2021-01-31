700cd34d991633045268f3f3c31208da565f5e56
kmarks2013 <kmarks2013@gmail.com>
2021-01-31 17:46:30 -0500

    adds random color generator for color block

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -10,6 +10,14 @@ const ColorScreen = () => {
     )
 }
 
+const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
 export default ColorScreen
 
 const styles = StyleSheet.create({})
