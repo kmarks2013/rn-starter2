be7cd25e572ce8da4e755a21d1ca57d500cc57a3
kmarks2013 <kmarks2013@gmail.com>
2021-01-31 16:55:08 -0500

    adds conter variable to add incorrect state example

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
 
 const  CounterScreen = () => {
+    // incorrect way to handle state
+    let counter= 0
+
     return (
         <View>
+            <Button title="Increase" />
+            <Button title="Decrease" />
             <Text>Current Count: 0</Text>
         </View>
     )
