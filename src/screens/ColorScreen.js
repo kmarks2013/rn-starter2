9b7de55dfb237ce44e60ad95fdcdb96009567900
kmarks2013 <kmarks2013@gmail.com>
2021-01-31 17:40:26 -0500

    adds broiler plate render with a button for color screen

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Button, View } from 'react-native'
+
+const ColorScreen = () => {
+    return (
+        <View>
+            <Button title="Add a Color" />
+        </View>
+    )
+}
+
+export default ColorScreen
+
+const styles = StyleSheet.create({})
