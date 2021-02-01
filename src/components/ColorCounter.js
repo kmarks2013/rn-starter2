09f5c4ea4b35cc5952098c84362e60fecd342f87
kmarks2013 <kmarks2013@gmail.com>
2021-02-01 15:03:10 -0500

    destructres color prop

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 
-const ColorCounter = () => {
+const ColorCounter = ({ color }) => {
     return (
         <View>
             <Text>Red</Text>
