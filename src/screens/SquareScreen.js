1dd03a8072c26aa93998bec33e2987187d3aceb8
kmarks2013 <kmarks2013@gmail.com>
2021-02-01 17:07:51 -0500

    updates change increments

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -12,19 +12,19 @@ const SquareScreen = () => {
     return (
         <View>
             <ColorCounter
-                onIncrease={() => setRed( red + 1 )}
-                onDecrease={() => setRed( red - 1 )}
+                onIncrease={() => setRed( red + COLOR_INCREMENT )}
+                onDecrease={() => setRed( red - COLOR_INCREMENT )}
                 color="Red"
             />
             <ColorCounter
                 color="Green"
-                onIncrease={() => setGreen( green + 1 )}
-                onDecrease={() => setGreen( green - 1 )}
+                onIncrease={() => setGreen( green + COLOR_INCREMENT )}
+                onDecrease={() => setGreen( green - COLOR_INCREMENT )}
             />
             <ColorCounter
                 color="Blue"
-                onIncrease={() => setBlue( blue + 1 )}
-                onDecrease={() => setBlue( blue - 1 )}
+                onIncrease={() => setBlue( blue + COLOR_INCREMENT )}
+                onDecrease={() => setBlue( blue - COLOR_INCREMENT )}
             />
             <View style={{ height:150, width:150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
         </View>
