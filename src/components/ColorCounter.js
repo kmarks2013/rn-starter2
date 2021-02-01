58a24440c9c1f13d16fbb2d28cca72859f7cb093
kmarks2013 <kmarks2013@gmail.com>
2021-02-01 14:56:25 -0500

    adds broiler plate for color counter

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ColorCounter = () => {
+    return (
+        <View>
+            <Text>counter of the color</Text>
+        </View>
+    )
+}
+
+export default ColorCounter
+
+const styles = StyleSheet.create({})
