a6a88c0b745aa783188d6636ea348639b6733122
kmarks2013 <kmarks2013@gmail.com>
2021-02-01 17:25:54 -0500

    updates callbacks for blue and green

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -60,13 +60,13 @@ const SquareScreen = () => {
             />
             <ColorCounter
                 color="Green"
-                onIncrease={() => setGreen( green + COLOR_INCREMENT )}
-                onDecrease={() => setGreen( green - COLOR_INCREMENT )}
+                onIncrease={() => setColor('green', COLOR_INCREMENT)}
+                onDecrease={() => setColor('green', -1 *COLOR_INCREMENT )}
             />
             <ColorCounter
                 color="Blue"
-                onIncrease={() => setBlue( blue + COLOR_INCREMENT )}
-                onDecrease={() => setBlue( blue - COLOR_INCREMENT )}
+                onIncrease={() => setColor('blue', COLOR_INCREMENT )}
+                onDecrease={() => setColor('blue', -1 *COLOR_INCREMENT )}
             />
             <View style={{ height:150, width:150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
         </View>
