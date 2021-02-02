5f6d1ac0e18954613f04937e1ffaa91d167bb780
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 14:58:39 -0500

    adds reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,6 +5,9 @@ import ColorCounter from '../components/ColorCounter'
 const COLOR_INCREMENT = 15
 
 const SquareScreen = () => {
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue:0})
+
+
     return (
         <View>
             <ColorCounter
