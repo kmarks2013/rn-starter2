ffedfcb5c140a779c509993ae095478d1c7242c1
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 16:46:04 -0500

    adds onchange text to update state of name

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -14,6 +14,7 @@ const TextScreen = () => {
                 autoCapitalize="none"
                 autoCorrect={false}
                 value={name}
+                onChangeText={(newText) => setName(newText) }
             />
         </View>
     )
