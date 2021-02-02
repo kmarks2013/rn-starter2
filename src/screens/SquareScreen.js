18fefb21aa886f88bd801a5e8b8d2cb937d21a17
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 15:26:16 -0500

    adds colors from state as variables

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -23,6 +23,7 @@ const reducer = (state, action) => {
 const SquareScreen = () => {
     const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue:0})
     // console.log(state)
+    const { red, green, blue } = state
 
     return (
         <View>
