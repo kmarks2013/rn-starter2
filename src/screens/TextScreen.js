62d25741a272a74a8fac1393f328af405b9ef211
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 16:19:34 -0500

    adds boiler plate render

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TextScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default TextScreen
+
+const styles = StyleSheet.create({})
