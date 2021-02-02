30593318a87a9034e081a70ccd5ee339c045cd3a
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 16:21:58 -0500

    imports TextInput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 
 const TextScreen = () => {
     return (
