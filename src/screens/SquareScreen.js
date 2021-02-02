6c1201f88b7314ea9b09a37ad93df8bc19dff921
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 15:55:08 -0500

    updates dispatch statements to match swithc statement

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -43,19 +43,19 @@ const SquareScreen = () => {
                 //     if (red + COLOR_INCREMENT > 255)
                 //         setRed( red + COLOR_INCREMENT)
                 // }}
-                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT})}
-                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => dispatch({ type: 'red', amount: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({ type: 'red', amount: -1 * COLOR_INCREMENT })}
                 color="Red"
             />
             <ColorCounter
                 color="Green"
-                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT})}
-                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
+                onIncrease={() => dispatch({ type: 'green', amount: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({ type: 'green', amount: -1 * COLOR_INCREMENT})}
             />
             <ColorCounter
                 color="Blue"
-                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
-                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT} )}
+                onIncrease={() => dispatch({ type: 'blue', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ type: 'blue', amount: -1 * COLOR_INCREMENT} )}
             />
             <View style={{ height:150, width:150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
         </View>
