ff42a09cc094996e7a30331b80d22f85897cecb7
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 16:00:09 -0500

    updates view for readabilty

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -52,7 +52,13 @@ const SquareScreen = () => {
                 onIncrease={() => dispatch({ type: 'change_blue', payload: COLOR_INCREMENT })}
                 onDecrease={() => dispatch({ type: 'change_blue', payload: -1 * COLOR_INCREMENT} )}
             />
-            <View style={{ height:150, width:150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
+            <View style={{
+                height:150,
+                width:150,
+                backgroundColor:
+                    `rgb(${red}, ${green}, ${blue})`
+                }}
+            />
         </View>
     )
 }
