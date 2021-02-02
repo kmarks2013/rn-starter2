25ab8f5228a56c62cb33076a112dae8570c8753d
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 14:57:31 -0500

    updates spacing errors

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({})
 //     const [blue, setBlue] = useState(0)
 
 //     // const setColor = (color, change) => {
-//         //     // color === 'red', 'green', 'blue
+//         //       // color === 'red', 'green', 'blue
 //         //     // change === +15, -15
 //         //     if (color === 'red'){
 //             //         if (red + change > 255 || red + change < 0) {
@@ -71,21 +71,21 @@ const styles = StyleSheet.create({})
 //                                     //     }
 //                                     // }
                                     
-//                                     const setColor = (color, change) => {
-//                                         switch (color) {
-//             case 'red':
-//                 red + change > 255 || red + change < 0 ? null : setRed(red + change)
-//                 return
-//                 case 'green':
-//                     green + change > 255 || green + change < 0 ? null : setGreen(green + change)
-//                     return
-//                     case 'blue':
-//                         blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
-//                         return
-//                         default:
-//                             return
-//                         }
-//                     }
+    // const setColor = (color, change) => {
+    //     switch (color) {
+    //         case 'red':
+    //             red + change > 255 || red + change < 0 ? null : setRed(red + change)
+    //             return
+    //         case 'green':
+    //             green + change > 255 || green + change < 0 ? null : setGreen(green + change)
+    //             return
+    //         case 'blue':
+    //             blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+    //             return
+    //         default:
+    //             return
+    //     }
+    // }
                     
 //     return (
 //         <View>
