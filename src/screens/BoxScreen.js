aa694e7592355238080cf117a46c6a7ea9e1355c
kmarks2013 <kmarks2013@gmail.com>
2021-02-05 19:45:05 -0500

    adds fill object styling

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -48,6 +48,7 @@ const styles = StyleSheet.create({
         // bottom: 0,
         // left: 0,
         // right: 0
+        ...StyleSheet.absoluteFillObject
     },
     textThreeStyle: {
         borderWidth: 3,
