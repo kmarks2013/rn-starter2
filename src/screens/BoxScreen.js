c2b50ff8b7bfde44411c5e4db2258715153bc31a
kmarks2013 <kmarks2013@gmail.com>
2021-02-05 19:29:47 -0500

    sets align self to felx end to show it over-ridding align items

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
         borderWidth: 3,
         borderColor: 'red',
         // flex:4 // <-- cuases elelemnt to take up as much space as possible in the parent
-        alignSelf: 'center'
+        alignSelf: 'flex-end'
     },
     textThreeStyle: {
         borderWidth: 3,
