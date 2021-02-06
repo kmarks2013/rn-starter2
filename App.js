3e4892b0030fc85e27973fa8b385b90ec8b2f16e
kmarks2013 <kmarks2013@gmail.com>
2021-02-05 19:56:49 -0500

    adds import for layout example screen

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import ColorScreen from './src/screens/ColorScreen';
 import SquareScreen from './src/screens/SquareScreen';
 import TextScreen from './src/screens/TextScreen';
 import BoxScreen from './src/screens/BoxScreen';
+import LayoutExampleScreen from './src/screens/LayoutExampleScreen';
 
 const navigator = createStackNavigator(
   {
