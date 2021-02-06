c2a1d8184091d0e25128448507ec3f27df88f678
kmarks2013 <kmarks2013@gmail.com>
2021-02-05 19:56:13 -0500

    adds boiler plate for layout example

---
+++ src/screens/LayoutExampleScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const LayoutExampleScreen = () => {
+    return (
+        <View>
+            <Text>Layout Example</Text>
+        </View>
+    )
+}
+
+export default LayoutExampleScreen
+
+const styles = StyleSheet.create({})
